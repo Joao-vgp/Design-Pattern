a17c03aee48c37b24032c99afe3b631e2e580bae
João <165852368+Joao-vgp@users.noreply.github.com>
2024-10-01 22:14:00 -0300

    Update CalculoMedia.java

--- Design_Pattern/Strategy/CalculoMedia.java
+++ Design_Pattern/Strategy/CalculoMedia.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package strategy;
 
-/**
- *
- * @author João Victor
- */
 public interface CalculoMedia {
+    public double calcular(double p1, double p2);
     
+    public String verificarAprovacao(double media);
 }
